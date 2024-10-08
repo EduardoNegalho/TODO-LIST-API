f13ee2f70833ab4720fabfb65c08793be795e901
Eduardo Marques <eduardomar1703@gmail.com>
2024-10-08 20:49:40 -0300

    adiciona operação para atualização da tarefa

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -26,3 +26,40 @@ export const createTask = async (req, res) => {
         return res.status(500).json({ erro: "Erro interno no servidor." });
     }
 }
+
+export const updateTask = async (req, res) => {
+    // valida dados de entrada
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() })
+    }
+
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    try {
+        // verifica se tarefa existe
+        const task = await prisma.task.findUnique({ 
+            where: { id: id } 
+        });
+        if (!task) { return res.status(404).json({ erro: "Tarefa não encontrada." }) }
+
+        // atualiza tarefa
+        await prisma.task.update({
+            where: {
+                id: id
+            },
+            data: {
+                title: title,
+                description: description,
+                status: status
+            }
+        })
+
+        return res.status(200).json({ message: "Tarefa atualizada com sucesso." });
+
+    } catch (error) {
+        console.error(`Error ao tentar atualizar a tarefa: ${error.message}`);
+        return res.status(500).json({ erro: "Erro interno no servidor." });
+    }
+}
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTask } from "../controllers/todoController.js";
+import { createTask, updateTask } from "../controllers/todoController.js";
 import validateTask from "../utils/taskValidation.js";
 
 const router = express.Router();
 
 router.post('/task', validateTask, createTask);
+router.put('/task/:id', validateTask, updateTask);
 
 export default router;
