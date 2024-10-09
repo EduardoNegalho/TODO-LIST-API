29037782d79d7de4a7e321b1fa76837f9f76eaae
Eduardo Marques <eduardomar1703@gmail.com>
2024-10-09 17:31:54 -0300

    adiciona operação para deletar uma tarefa

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -23,7 +23,7 @@ export const createTask = async (req, res) => {
         return res.status(201).json({ message: "Tarefa criada com sucesso." });
     } catch (error) {
         console.error(`Erro ao criar tarefa: ${error.message}`);
-        return res.status(500).json({ erro: "Erro interno no servidor." });
+        return res.status(500).json({ error: "Erro interno no servidor." });
     }
 }
 
@@ -42,7 +42,7 @@ export const updateTask = async (req, res) => {
         const task = await prisma.task.findUnique({
             where: { id: id }
         });
-        if (!task) { return res.status(404).json({ erro: "Tarefa não encontrada." }) }
+        if (!task) { return res.status(404).json({ error: "Tarefa não encontrada." }) }
 
         // atualiza tarefa
         await prisma.task.update({
@@ -60,7 +60,7 @@ export const updateTask = async (req, res) => {
 
     } catch (error) {
         console.error(`Error ao tentar atualizar a tarefa: ${error.message}`);
-        return res.status(500).json({ erro: "Erro interno no servidor." });
+        return res.status(500).json({ error: "Erro interno no servidor." });
     }
 }
 
@@ -73,12 +73,45 @@ export const getTasks = async (req, res) => {
 
     try {
         const tasks = await prisma.task.findMany({
-            where: query
+            where: query,
+            select: {
+                title: true,
+                description: true,
+                status: true
+            }
         });
+
+        if (tasks.length === 0) {
+            return res.status(404).json({ message: "Nenhuma tarefa encontrada" });
+        }
 
         return res.status(200).json(tasks);
     } catch (error) {
         console.error(`Erro ao tentar buscar as tarefas: ${error.message}`);
-        return res.status(500).json({ erro: "Erro interno no servidor." });
+        return res.status(500).json({ error: "Erro interno no servidor." });
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // verifica se tarefa existe
+        const task = await prisma.task.findUnique({
+            where: { id: id }
+        });
+        if (!task) { return res.status(404).json({ error: "Tarefa não encontrada." }) }
+
+        // exclui tarefa do banco de dados
+        await prisma.task.delete({
+            where: {
+                id: id
+            }
+        })
+
+        return res.status(200).json({ message: "Tarefa excluída com sucesso." });
+    } catch (error) {
+        console.error(`Erro ao tentar excluir a tarefa: ${error.message}`);
+        return res.status(500).json({ error: "Erro interno no servidor." });
     }
 }
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask, updateTask, getTasks } from "../controllers/todoController.js";
+import { createTask, updateTask, getTasks, deleteTask } from "../controllers/todoController.js";
 import validateTask from "../utils/taskValidation.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/task', validateTask, createTask);
 router.put('/task/:id', validateTask, updateTask);
 router.get('/task/', getTasks);
+router.delete('/task/:id', deleteTask);
 
 export default router;
